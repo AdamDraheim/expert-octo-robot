494d43f5f867fb602145befc71fa4cc3c18fdaa8
Adam Draheim <acdraheim@gmail.com>
2018-06-13 21:30:10 -0600

    Base Objects to use in Game

---
+++ Octo Robot/Assets/_Script/CameraAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAction : MonoBehaviour {
+
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = player.gameObject.transform.position;
+        gameObject.transform.position += new Vector3(0, 10, 0);
+
+    }
+}
---
+++ Octo Robot/Assets/_Script/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Vector3 Velocity;
+    private float theta = 0;
+
+	// Use this for initialization
+	void Start () {
+        Velocity = new Vector3(1, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        Velocity = new Vector3(Mathf.Sin(theta), 0, Mathf.Cos(theta));
+        Velocity *= Time.deltaTime * 5;
+        gameObject.transform.position += Velocity;
+        if (Input.GetKey(KeyCode.D))
+        {
+            theta += 5 * Time.deltaTime;
+            gameObject.transform.Rotate(Vector3.right * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            theta -= 5 * Time.deltaTime;
+            gameObject.transform.Rotate(Vector3.left * Time.deltaTime);
+
+        }
+
+
+    }
+}
